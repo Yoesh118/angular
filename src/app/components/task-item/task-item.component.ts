5673c3c355ba7257c61545b0fc1d72d7b4269472
yoesh118 <makwarayoesh@gmail.com>
2021-10-06 13:49:58 +0200

    delete function, json server

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from 'src/app/Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-task-item',
@@ -8,10 +9,15 @@ import { Task } from 'src/app/Task';
 })
 export class TaskItemComponent implements OnInit {
   @Input() task:Task
+  @Output() deleteTask: EventEmitter<Task>= new EventEmitter()
+  faTimes = faTimes;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  delete(task){
+    this.deleteTask.emit(task);
   }
  
 }
